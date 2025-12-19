82412051079f2a7311551c104015dffa37eb73fd
chuthamat <chuthamatbuaban@gmail.com>
2025-12-19 16:39:32 +0700

    Fix name base respository

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Todo-List-React/",
   plugins: [react(), tailwindcss()],
 });
