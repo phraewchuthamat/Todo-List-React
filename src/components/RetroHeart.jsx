159b4af2c855d6f07eae27fd832af70742dee071
chuthamat <chuthamatbuaban@gmail.com>
2026-01-15 11:35:52 +0700

    fix floder image heart-pixel to assets

--- src/components/RetroHeart.jsx
+++ src/components/RetroHeart.jsx
@@ -1,10 +1,12 @@
+import heartPixel from '../assets/heart-pixel.png'
+
 export const RetroHeart = ({ className = '' }) => {
     return (
         <div
             className={`inline-block filter drop-shadow-[4px_4px_0px_rgba(0,0,0,1)] ${className}`}
         >
             <img
-                src="public/image/heart-pixel.png"
+                src={heartPixel}
                 alt="Pixel Heart"
                 className="w-12 h-12 animate-heartbeat object-contain"
                 draggable="false"
