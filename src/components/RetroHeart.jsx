5d8f4ecf702f797248ea1c74c00151c691418d1a
chuthamat <chuthamatbuaban@gmail.com>
2026-01-15 11:25:40 +0700

    fix address src img in REtroHeart.jsx

--- src/components/RetroHeart.jsx
+++ src/components/RetroHeart.jsx
@@ -4,7 +4,7 @@ export const RetroHeart = ({ className = '' }) => {
             className={`inline-block filter drop-shadow-[4px_4px_0px_rgba(0,0,0,1)] ${className}`}
         >
             <img
-                src="public\image\heart-pixel.png"
+                src="../../public//image/heart-pixel.png"
                 alt="Pixel Heart"
                 className="w-12 h-12 animate-heartbeat object-contain"
                 draggable="false"
